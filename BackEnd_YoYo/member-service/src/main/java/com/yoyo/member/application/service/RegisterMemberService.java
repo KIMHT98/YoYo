a7aa2cbe2b8a7b165eea2b5345d88244a2167bc5
최광림 <choikl3115@naver.com>
2024-09-06 17:56:01 +0900

    BE Feat: Docker-kafka

--- BackEnd_YoYo/member-service/src/main/java/com/yoyo/member/application/service/RegisterMemberService.java
+++ BackEnd_YoYo/member-service/src/main/java/com/yoyo/member/application/service/RegisterMemberService.java
@@ -10,6 +10,7 @@ import com.yoyo.member.domain.Member;
 import com.yoyo.member.domain.Member.MemberBirthDay;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Transactional
@@ -18,13 +19,15 @@ public class RegisterMemberService implements RegisterMemberUseCase {
 
     private final RegisterMemberPort registerMemberPort;
     private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public Member registerMember(RegisterMemberCommand command) {
         // command -> DB
         // port, adapter
+        String encodedPassword = passwordEncoder.encode(command.getPassword());
         MemberJpaEntity jpaEntity = registerMemberPort.createMember(
                 new Member.MemberName(command.getName()),
-                new Member.MemberPassword(command.getPassword()),
+                new Member.MemberPassword(encodedPassword),
                 new Member.MemberPhoneNumber(command.getPhoneNumber()),
                 new MemberBirthDay(command.getBirthDay())
         );
