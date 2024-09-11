cec239925c20615602a5d3b6d067b628b39cdac1
이찬진 <cj8291@naver.com>
2024-09-11 15:23:22 +0900

    refactor: modify yoyocard

--- FrontEnd_YoYo/components/card/Card.jsx
+++ FrontEnd_YoYo/components/card/Card.jsx
@@ -27,6 +27,7 @@ const styles = StyleSheet.create({
         overflow: "hidden",
         elevation: 5,
         backgroundColor: "white",
+        marginVertical: 8,
     },
     pressableContainer: {
         borderRadius: 16,
--- FrontEnd_YoYo/pages/giveAndtake/regist/GiveAndTakeRegist.jsx
+++ FrontEnd_YoYo/pages/giveAndtake/regist/GiveAndTakeRegist.jsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function GiveAndTakeRegist() {
-  return (
-    <View>
-      <Text>GiveAndTakeRegist</Text>
-    </View>
-  )
+export default function GiveAndTakeRegist({ navigator }) {
+    return (
+        <View>
+            <Text>GiveAndTakeRegist</Text>
+        </View>
+    );
 }
