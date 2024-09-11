cd6189aafcb0798cca112fb8e0ad5ba3ba3652ee
이찬진 <cj8291@naver.com>
2024-09-11 09:11:45 +0900

    feat: add give and take list page

--- FrontEnd_YoYo/pages/giveAndtake/list/GiveAndTakeList.jsx
+++ FrontEnd_YoYo/pages/giveAndtake/list/GiveAndTakeList.jsx
@@ -1,25 +1,28 @@
-import {
-    View,
-    Text,
-    ScrollView,
-    SafeAreaView,
-    StyleSheet,
-    Pressable,
-} from "react-native";
+import { View, StyleSheet, Pressable, FlatList } from "react-native";
 import React from "react";
 import Container from "../../../components/common/Container";
 import YoYoCard from "../../../components/card/Yoyo/YoYoCard";
-import YoYoCardDetail from "../../../components/card/Yoyo/YoYoCardDetail";
 import SearchBar from "../../../components/common/SearchBar";
 import Tag from "../../../components/common/Tag";
 import YoYoText from "../../../constants/YoYoText";
-import IconButton from "../../../components/common/IconButton";
 import { MainStyle } from "../../../constants/style";
 import Button from "../../../components/common/Button";
 import { useNavigation } from "@react-navigation/native";
 
 export default function GiveAndTakeList() {
     const navigation = useNavigation();
+    const margin = 2;
+
+    const DATA = [
+        {
+            id: "1",
+            title: "First Item",
+        },
+        {
+            id: "2",
+            title: "Second Item",
+        },
+    ];
 
     const clickNextHandler = () => {
         navigation.navigate("GiveAndTakeRegist");
@@ -34,42 +37,38 @@ export default function GiveAndTakeList() {
                 <View style={styles.innerContainer}>
                     <SearchBar placeholder={"이름을 검색해 주세요."} />
                 </View>
-                <SafeAreaView style={styles.innerContainer}>
-                    <ScrollView
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
-                    >
-                        <Pressable onPress={tagHandler}>
-                            <Tag type="all" margin={5}>
-                                <YoYoText bold>전체</YoYoText>
-                            </Tag>
-                        </Pressable>
-                        <Pressable onPress={tagHandler}>
-                            <Tag type="family" margin={5}>
-                                <YoYoText bold>가족</YoYoText>
-                            </Tag>
-                        </Pressable>
-                        <Pressable onPress={tagHandler}>
-                            <Tag type="friend" margin={5}>
-                                <YoYoText bold>친구</YoYoText>
-                            </Tag>
-                        </Pressable>
-                        <Pressable onPress={tagHandler}>
-                            <Tag type="company" margin={5}>
-                                <YoYoText bold>직장</YoYoText>
-                            </Tag>
-                        </Pressable>
-                        <Pressable onPress={tagHandler}>
-                            <Tag type="etc" margin={5}>
-                                <YoYoText bold>기타</YoYoText>
-                            </Tag>
-                        </Pressable>
-                    </ScrollView>
-                </SafeAreaView>
-                <ScrollView style={styles.innerContainer}>
-                    <YoYoCard />
-                    <YoYoCard />
-                </ScrollView>
+                <View style={styles.tagContainer}>
+                    <Pressable onPress={tagHandler}>
+                        <Tag type="all" width={64}>
+                            <YoYoText bold>전체</YoYoText>
+                        </Tag>
+                    </Pressable>
+                    <Pressable onPress={tagHandler}>
+                        <Tag type="family" width={64}>
+                            <YoYoText bold>가족</YoYoText>
+                        </Tag>
+                    </Pressable>
+                    <Pressable onPress={tagHandler}>
+                        <Tag type="friend" width={64}>
+                            <YoYoText bold>친구</YoYoText>
+                        </Tag>
+                    </Pressable>
+                    <Pressable onPress={tagHandler}>
+                        <Tag type="company" width={64}>
+                            <YoYoText bold>직장</YoYoText>
+                        </Tag>
+                    </Pressable>
+                    <Pressable onPress={tagHandler}>
+                        <Tag type="etc" width={64}>
+                            <YoYoText bold>기타</YoYoText>
+                        </Tag>
+                    </Pressable>
+                </View>
+                <FlatList
+                    data={DATA}
+                    renderItem={({}) => <YoYoCard />}
+                    style={styles.innerContainer}
+                ></FlatList>
             </Container>
             <Button type={"fill"} onPress={clickNextHandler}>
                 <YoYoText bold>거래내역 추가하기</YoYoText>
@@ -80,6 +79,11 @@ export default function GiveAndTakeList() {
 
 const styles = StyleSheet.create({
     innerContainer: {
+        marginBottom: 16,
+    },
+    tagContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
         marginBottom: 16,
     },
     iconContainer: {
