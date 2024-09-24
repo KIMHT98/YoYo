e65f7abc60afbe2ace3110cc30904640d93aefe5
kht19684 <kht19684@gmail.com>
2024-09-24 16:39:35 +0900

    FEAT:로그인_회원가입api완료

--- FrontEnd_YoYo/apis/axiosConstants.js
+++ FrontEnd_YoYo/apis/axiosConstants.js
@@ -1,3 +1,5 @@
-export const BASE_URL = (port) => {
-    return `localhost:${port}/yoyo/`;
+export const BASE_URL = `http://70.12.247.36:8000/yoyo/`;
+
+export const END_POINT = {
+    MEMBER: (path) => `members/${path}`,
 };
--- FrontEnd_YoYo/apis/axiosInstance.js
+++ FrontEnd_YoYo/apis/axiosInstance.js
@@ -1,15 +1,5 @@
 import axios from "axios";
 import { BASE_URL } from "./axiosConstants";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-export const axiosInstance = async (port) => {
-    const token = await AsyncStorage.getItem("token");
-
-    return axios.create({
-        baseURL: BASE_URL(port),
-        headers: {
-            Authorization: token ? `Bearer ${token}` : null,
-        },
-        withCredentials: true,
-    });
-};
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+});
