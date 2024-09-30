ce92884ac86643e0bbf05591a7c3d225655e3ebe
jhw <7115gpdnjs@naver.com>
2024-09-30 10:24:01 +0900

    feat/BE : pin 번호 암호화 방식 변경

--- BackEnd_YoYo/banking-service/src/main/java/com/yoyo/banking/domain/account/dto/account/AccountCreateDTO.java
+++ BackEnd_YoYo/banking-service/src/main/java/com/yoyo/banking/domain/account/dto/account/AccountCreateDTO.java
@@ -1,15 +1,16 @@
 package com.yoyo.banking.domain.account.dto.account;
 
 import com.yoyo.banking.entity.Account;
-import com.yoyo.common.util.PasswordEncryptUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class AccountCreateDTO {
 
@@ -31,17 +32,21 @@ public class AccountCreateDTO {
         @AssertTrue(message = "인증되지 않은 계좌입니다.")
         @Schema(description = "1원 송금 인증 여부 : true만 허용")
         private Boolean isAuthenticated;
-        @NotNull(message = "결제 비밀번호를 입력하세요.")
+        @NotBlank(message = "결제 비밀번호를 입력하세요.")
+        @Pattern(regexp = "^[0-9]{6}$", message = "6자리 숫자만 입력할 수 있습니다.")
+        @Schema(description = "결제 비밀번호 : 6자리 숫자만 입력가능")
         private String pin;
 
         // TODO : 계좌번호 저장시 양방향 암호화 알고리즘 적용 필요
         public static Account toEntity(AccountCreateDTO.Request request, Long memberId, String bankCode) {
+            String hashedPin = BCrypt.hashpw(request.getPin(), BCrypt.gensalt());
+
             return Account.builder()
                           .memberId(memberId)
                           .accountNumber(request.getAccountNumber())
                           .bankCode(bankCode)
                           .balance(0L)
-                          .pin(PasswordEncryptUtil.encrypt(request.getPin()))
+                          .pin(hashedPin)
                           .build();
         }
     }
