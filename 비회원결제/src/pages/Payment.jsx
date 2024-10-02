aa91dfbef86786ef7b4718bdf864ff796fcdcf91
kht19684 <kht19684@gmail.com>
2024-10-02 17:01:53 +0900

    FE Feat : 비회원 데이터

--- 비회원결제/src/pages/Payment.jsx
+++ 비회원결제/src/pages/Payment.jsx
@@ -1,17 +1,22 @@
-import { loadTossPayments, ANONYMOUS } from "@tosspayments/tosspayments-sdk";
-import { useEffect, useState, useNavigate } from "react";
+import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import '../App.css'
 const clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq";
 const customerKey = generateRandomString();
 
 export function Payment() {
+
   const [payment, setPayment] = useState(null);
   const navigate = useNavigate();
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
-  const [phoneNumber, setPhoneNumber] = useState("");
   // 두개 값 보내기 /payment/success
   const [senderName, setSenderName] = useState("");
+  const [price, setPrice] = useState("")
   const [description, setDescription] = useState("");
-
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const { id } = useParams();
+  console.log(id)
   function selectPaymentMethod(method) {
     setSelectedPaymentMethod(method);
   }
@@ -32,6 +37,7 @@ export function Payment() {
     fetchPayment();
   }, [clientKey, customerKey]);
 
+  // 
   async function requestPayment() {
     const amount = {
       currency: "KRW",
@@ -39,50 +45,51 @@ export function Payment() {
     };
 
     try {
+      let successUrl = window.location.origin + "/payment/success";
+      let failUrl = window.location.origin + "/fail";
+
       switch (selectedPaymentMethod) {
-      case "CARD":
-        await payment.requestPayment({
-          method: "CARD", // 카드 및 간편결제
-          amount,
-          orderId: generateRandomString(), // 받는 사람 + 고유 주문번호
-          orderName: description,
-          successUrl: window.location.origin + "/payment/success", 
-          failUrl: window.location.origin + "/fail",
-          customerEmail: "",
-          customerName: "",
-          customerMobilePhone: phoneNumber,
-          card: {
-            useEscrow: false,
-            flowMode: "DEFAULT",
-            useCardPoint: false,
-            useAppCardOnly: false,
-          },
-        });
-        break; 
-      case "TRANSFER":
-        await payment.requestPayment({
-          method: "TRANSFER", // 계좌이체 결제
-          amount, 
-          orderId: generateRandomString(),
-          orderName: "마음 보내기",
-          successUrl: window.location.origin + "/payment/success",
-          failUrl: window.location.origin + "/fail",
-          customerEmail: "",
-          customerName: "",
-          customerMobilePhone: phoneNumber,
-          transfer: {
-            cashReceipt: {
-              type: "소득공제",
+        case "CARD":
+          await payment.requestPayment({
+            method: "CARD",
+            amount,
+            orderId: generateRandomString(),
+            orderName: description,
+            successUrl: `${successUrl}?senderName=${senderName}&description=${description}&eventId=${id}`, // URL에 쿼리 스트링으로 전달
+            failUrl,
+            customerEmail: "",
+            customerName: "",
+            customerMobilePhone: phoneNumber,
+            card: {
+              useEscrow: false,
+              flowMode: "DEFAULT",
+              useCardPoint: false,
+              useAppCardOnly: false,
             },
-            useEscrow: false,
-          },
-        });
-        break;
-      default:
-        alert("결제 수단을 선택해주세요.");
+          });
+          break;
+        case "TRANSFER":
+          await payment.requestPayment({
+            method: "TRANSFER",
+            amount,
+            orderId: generateRandomString(),
+            orderName: "마음 보내기",
+            successUrl: `${successUrl}?senderName=${senderName}&description=${description}&eventId=${id}`, // URL에 쿼리 스트링으로 전달
+            failUrl,
+            customerEmail: "",
+            customerName: "",
+            customerMobilePhone: phoneNumber,
+            transfer: {
+              cashReceipt: {
+                type: "소득공제",
+              },
+              useEscrow: false,
+            },
+          });
+          break;
+        default:
+          alert("결제 수단을 선택해주세요.");
       }
-      // "/payment/success" 페이지로 값 보내기
-navigate("/payment/success", { state: { senderName: senderName, description: description } });
     }
     catch (error) {
       console.log("결제 에러", error);
@@ -92,43 +99,49 @@ navigate("/payment/success", { state: { senderName: senderName, description: des
   return (
     <div className="wrapper">
       <div className="box_section">
-        <h1>비회원 결제</h1>
-        <div>
-          <label>보내는 사람 이름:</label>
+        <h1>마음 전하기</h1>
+        <div className="inputContainer">
+          <label className="inputLabel">보내시는 분</label>
           <input
+            className="infoInput"
             type="text"
             value={senderName}
             onChange={(e) => setSenderName(e.target.value)}
             placeholder="보내는 사람 이름을 입력하세요"
           />
         </div>
-        <div>
-          <label>가격:</label>
+        <div className="inputContainer">
+          <label className="inputLabel">금액을 입력해주세요.</label>
           <input
-            type="number"
+            className="infoInput"
+            type="text"
             value={price}
             onChange={(e) => setPrice(Number(e.target.value))}
             placeholder="가격을 입력하세요"
           />
         </div>
-        <div>
-          <label>전화번호:</label>
-          <input
-            type="tel"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            placeholder="전화번호를 입력하세요"
-          />
-        </div>
-        <div>
-          <label>메모:</label>
-          <input
+        <div className="inputContainer">
+          <label className="inputLabel">주최자와의 관계</label>
+          <textarea
+            className="infoArea"
             type="text"
             onChange={(e) => setDescription(e.target.value)}
             placeholder="메모를 입력하세요"
           />
         </div>
-        <div id="payment-method" style={{ display: "flex", justifyContent:"center" }}>
+        <div className="inputContainer">
+          <label className="inputLabel">전화번호</label>
+          <input
+            className="infoInput"
+            type="tel"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="전화번호를 입력하세요"
+          />
+
+        </div>
+
+        <div id="payment-method" style={{ display: "flex", justifyContent: "center", width: "100%" }}>
           <button id="CARD" className={`button2 ${selectedPaymentMethod === "CARD" ? "active" : ""}`} onClick={() => selectPaymentMethod("CARD")}>
             카드
           </button>
