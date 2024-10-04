4a80a1f57c99dc610f356894fe77241c18ac81ed
jhw <7115gpdnjs@naver.com>
2024-10-04 11:05:47 +0900

    feat/BE : Enum값 검증을 위한 @ValidEnum 생성

---
+++ BackEnd_YoYo/common/src/main/java/com/yoyo/common/util/EnumValidator.java
@@ -0,0 +1,26 @@
+package com.yoyo.common.util;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Object> {
+
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues == null) return false;
+
+        for (Object enumValue : enumValues) {
+            if (enumValue.toString().equals(value))
+                return true;
+        }
+        return false;
+    }
+}
