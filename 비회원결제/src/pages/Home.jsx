90852462c56bc99c4f89ade7e41f715e8f79b07d
김현태 <kht19684@gmail.com>
2024-10-11 10:40:22 +0900

    Update 비회원결제/src/pages/Home.jsx

--- 비회원결제/src/pages/Home.jsx
+++ 비회원결제/src/pages/Home.jsx
@@ -7,10 +7,11 @@ export default function Home() {
         <h1>YoYo!</h1>
         <h3>경조사 비용 관리 어플리케이션</h3>
         <h4>YoYo를 설치하고 경조사 비용 관리를 자동으로 맡겨요!</h4>
-        <button className="button" onClick={() => {
+         <button className="button" onClick={() => {
           const userResponse = window.confirm("YoYo를 설치하시겠습니까?");
           if (userResponse) {
             alert("앱을 다운로드합니다.");
+            window.location.href = "https://expo.dev/artifacts/eas/pJHh1bzZbiBetzTMVaVVyh.apk";
           } else {
             return
           }
